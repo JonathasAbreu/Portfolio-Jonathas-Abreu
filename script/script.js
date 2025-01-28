12bcad2847b187e6009410067a8cf9376d97c3ad
unknown <jonathasabreupontes@gmail.com>
2025-01-28 14:58:41 -0300

    Atualizando arquitetura do portfolio

--- script/script.js
+++ script/script.js
@@ -1,3 +1,11 @@
+window.addEventListener('load', function() {
+    // Seleciona o body (ou outros elementos, se desejar)
+    const body = document.querySelector('body');
+
+    // Adiciona a classe 'ativo' após o carregamento da página
+    body.classList.add('ativo');
+});
+
 $("#year").text(new Date().getFullYear());
 
 $("body").scrollspy({ target: "#main-nav" });
@@ -30,3 +38,57 @@ document.querySelectorAll('.botaoMouse').forEach(function (botao) {
         }
     });
 });
+
+
+
+// Variáveis globais
+let modal = document.getElementById("imageModal");
+let modalImage = document.getElementById("modalImage");
+let images = document.querySelectorAll(".gallery-img");
+let currentIndex = 0;
+let navbar = document.getElementById('main-nav');  // Seleciona a navbar
+
+// Função para abrir o modal e exibir a imagem clicada
+images.forEach((img, index) => {
+    img.addEventListener("click", () => {
+        // Esconde a navbar ao abrir o modal
+        navbar.classList.add('hidden');  // Aplica a classe para esconder a navbar
+        
+        modal.style.display = "block";
+        modalImage.src = img.src;
+        currentIndex = index;  // Atualiza o índice da imagem clicada
+    });
+});
+
+// Função para fechar o modal ao clicar no "X"
+document.querySelector(".close").onclick = function() {
+    // Mostra a navbar novamente ao fechar o modal
+    navbar.classList.remove('hidden');  // Remove a classe para mostrar a navbar
+    
+    modal.style.display = "none";
+};
+
+// Função para navegar entre as imagens
+function changeImage(direction) {
+    currentIndex += direction;
+    
+    // Garantir que o índice seja circular (loop infinito)
+    if (currentIndex < 0) {
+        currentIndex = images.length - 1;  // Vai para a última imagem
+    } else if (currentIndex >= images.length) {
+        currentIndex = 0;  // Vai para a primeira imagem
+    }
+
+    modalImage.src = images[currentIndex].src;  // Atualiza a imagem no modal
+}
+
+// Fechar o modal clicando fora da imagem (ou no fundo escuro)
+window.onclick = function(event) {
+    if (event.target === modal) {
+        // Mostra a navbar novamente
+        navbar.classList.remove('hidden');  // Remove a classe para mostrar a navbar
+        modal.style.display = "none";
+    }}
+
+
+    
